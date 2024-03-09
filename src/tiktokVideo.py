8f2defddfe005f51bedc661cb4642b87c726980d
2noScript <thangmk7@gmail.com>
2024-03-09 13:35:13 +0700

    [Update] base supplier video water mark

--- src/tiktokVideo.py
+++ src/tiktokVideo.py
@@ -3,11 +3,14 @@ from pyppeteer import launch
 from pyppeteer_stealth import stealth
 from bs4 import BeautifulSoup
 import re
+from pydash import uniq,sample
+
+from constants import WATER_MARK,BASE_URL
 
 
 class TikTokVideo:
     def __init__(self):
-        self.baseUrl='https://www.tiktok.com'
+        self.baseUrl=BASE_URL
 
     async def getContextByUser(self,userName):
         url=f'{self.baseUrl}/@{userName}'
@@ -22,14 +25,18 @@ class TikTokVideo:
     async def getVideoIdListByUser(self,userName):
         content=await self.getContextByUser(userName)
         soup = BeautifulSoup(content, 'html.parser')
-        pattern = re.compile(fr'https://www\.tiktok\.com/@{userName}/video/')
+        pattern = re.compile(fr'{self.baseUrl}/@{userName}/video/')  # check match is video url
         links = soup.find_all('a', href=pattern)
         data=[]
         for link in links:
             match=re.compile(r'(\w+)$').search(link['href'])
             data.append(match.group(1))
-        print(data)
-        print(len(data))
+        return uniq(data)
+    
+    def getUrlWaterMark(self,videId):
+        return f'https://{sample(WATER_MARK)}/aweme/v1/feed/?aweme_id={videId}'
+    
+
           
         
 
@@ -37,7 +44,9 @@ class TikTokVideo:
 
 async def main():
     vd = TikTokVideo()
-    await vd.getVideoIdListByUser('deykayana1')
+    print(vd.getUrlWaterMark('6951395225445158146'))
+
+
    
       
 
